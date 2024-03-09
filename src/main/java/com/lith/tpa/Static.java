f7bacbeab3b009a090087e9bdada70f6e1cb660f
ImLith <baranauskas.edvinas26@gmail.com>
2024-03-09 20:14:57 +0200

    init

---
+++ src/main/java/com/lith/tpa/Static.java
@@ -0,0 +1,8 @@
+package com.lith.tpa;
+
+import java.util.logging.Logger;
+
+public class Static {
+    public static final String pluginName = "TPA";
+    public static final Logger log = Logger.getLogger(Static.pluginName);
+}
